e819f028abf4b1b6d3f856d77776b2d512b632c3
annsad9 <sorokina.anna.09@mail.ru>
2025-09-04 20:45:37 +0500

    Update DimCustomer_Clear.sql
    OK

--- project_2/sql/DimCustomer_Clear.sql
+++ project_2/sql/DimCustomer_Clear.sql
@@ -1,16 +1,15 @@
 -- DimCustomer_Clear --
-SELECT 
-  c.[CustomerKey],
-  c.[FirstName], 
-  c.[LastName], 
-  c.[FirstName] + '' + c.[LastName] AS FullName, 
-  CASE c.[Gender] WHEN 'M' THEN 'Male' WHEN 'f' THEN 'Female' END AS Gender, 
-  c.[DateFirstPurchase], 
-  g.[City] AS CustomerCity 
-FROM 
-  [AdventureWorksDW2022].[dbo].[DimCustomer] AS c 
-  LEFT JOIN [AdventureWorksDW2022].[dbo].[DimGeography] AS g ON g.GeographyKey = c.GeographyKey 
-ORDER BY 
-  [CustomerKey] ASC
 
-
+SELECT c.[CustomerKey],
+       c.[FirstName],
+       c.[LastName],
+       c.[FirstName] + '' + c.[LastName] AS FullName,
+       CASE c.[Gender]
+           WHEN 'M' THEN 'Male'
+           WHEN 'f' THEN 'Female'
+       END AS Gender,
+       c.[DateFirstPurchase],
+       g.[City] AS CustomerCity
+FROM [AdventureWorksDW2022].[dbo].[DimCustomer] AS c
+LEFT JOIN [AdventureWorksDW2022].[dbo].[DimGeography] AS g ON g.GeographyKey = c.GeographyKey
+ORDER BY [CustomerKey] ASC
