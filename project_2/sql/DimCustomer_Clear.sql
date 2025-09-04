0706e114b95c43fedfe57e0c2b7b0a9fbe013d28
annsad9 <sorokina.anna.09@mail.ru>
2025-09-04 17:08:35 +0500

    add sql script

---
+++ project_2/sql/DimCustomer_Clear.sql
@@ -0,0 +1,38 @@
+-- Подготовленные данные DimCustomer
+SELECT 
+  c.[CustomerKey],
+  --[GeographyKey],
+  --[CustomerAlternateKey],
+  --[Title],
+  c.[FirstName], 
+  --[MiddleName],
+  c.[LastName], 
+  c.[FirstName] + '' + c.[LastName] AS FullName, 
+  --[NameStyle],
+  --[BirthDate],
+  --[MaritalStatus],
+  --[Suffix],
+  CASE c.[Gender] WHEN 'M' THEN 'Male' WHEN 'f' THEN 'Female' END, 
+  --[EmailAddress],
+  --[YearlyIncome],
+  --[TotalChildren],
+  --[NumberChildrenAtHome],
+  --[EnglishEducation],
+  --[SpanishEducation],
+  --[FrenchEducation],
+  --[EnglishOccupation],
+  --[SpanishOccupation],
+  --[FrenchOccupation],
+  --[HouseOwnerFlag],
+  --[NumberCarsOwned],
+  --[AddressLine1],
+  --[AddressLine2],
+  --[Phone],
+  c.[DateFirstPurchase], 
+  --[CommuteDistance],
+  g.[City] AS CustomerCity 
+FROM 
+  [AdventureWorksDW2022].[dbo].[DimCustomer] AS c 
+  LEFT JOIN [AdventureWorksDW2022].[dbo].[DimGeography] AS g ON g.GeographyKey = c.GeographyKey 
+ORDER BY 
+  [CustomerKey] ASC
---
+++ project_2/sql/FactInternetSales_Clear.sql
@@ -0,0 +1,33 @@
+-- Подготовленные данные FACT_InternetSales
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  --  ,[PromotionKey]
+  --  ,[CurrencyKey]
+  --  ,[SalesTerritoryKey]
+  [SalesOrderNumber], 
+  --  [SalesOrderLineNumber], 
+  --  ,[RevisionNumber]
+  --  ,[OrderQuantity], 
+  --  ,[UnitPrice], 
+  --  ,[ExtendedAmount]
+  --  ,[UnitPriceDiscountPct]
+  --  ,[DiscountAmount] 
+  --  ,[ProductStandardCost]
+  --  ,[TotalProductCost] 
+  [SalesAmount] --  ,[TaxAmt]
+  --  ,[Freight]
+  --  ,[CarrierTrackingNumber] 
+  --  ,[CustomerPONumber] 
+  --  ,[OrderDate] 
+  --  ,[DueDate] 
+  --  ,[ShipDate] 
+FROM 
+  [AdventureWorksDW2022].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 -- Всегда смотрим последнии два года
+ORDER BY
+  OrderDateKey ASC
