8826fc6a40444a6d9169d975ca2728d0cfb9e9a0
annsad9 <sorokina.anna.09@mail.ru>
2025-09-04 20:49:42 +0500

    Update FactInternetSales_Clear.sql
    OK

--- project_2/sql/FactInternetSales_Clear.sql
+++ project_2/sql/FactInternetSales_Clear.sql
@@ -1,35 +1,13 @@
 -- FactInternetSales_Clear --
-SELECT 
-  [ProductKey], 
-  [OrderDateKey], 
-  [DueDateKey], 
-  [ShipDateKey], 
-  [CustomerKey], 
-  --  ,[PromotionKey]
-  --  ,[CurrencyKey]
-  --  ,[SalesTerritoryKey]
-  [SalesOrderNumber], 
-  --  [SalesOrderLineNumber], 
-  --  ,[RevisionNumber]
-  --  ,[OrderQuantity], 
-  --  ,[UnitPrice], 
-  --  ,[ExtendedAmount]
-  --  ,[UnitPriceDiscountPct]
-  --  ,[DiscountAmount] 
-  --  ,[ProductStandardCost]
-  --  ,[TotalProductCost] 
-  [SalesAmount] --  ,[TaxAmt]
-  --  ,[Freight]
-  --  ,[CarrierTrackingNumber] 
-  --  ,[CustomerPONumber] 
-  --  ,[OrderDate] 
-  --  ,[DueDate] 
-  --  ,[ShipDate] 
-FROM 
-  [AdventureWorksDW2022].[dbo].[FactInternetSales]
-WHERE 
-  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 -- always look at the last two years
-ORDER BY
-  OrderDateKey ASC
 
-
+SELECT [ProductKey],
+       [OrderDateKey],
+       [DueDateKey],
+       [ShipDateKey],
+       [CustomerKey],
+       [SalesOrderNumber],
+       [SalesAmount]
+FROM [AdventureWorksDW2022].[dbo].[FactInternetSales]
+WHERE LEFT (OrderDateKey,
+            4) >= YEAR(GETDATE()) -2 -- always look at the last two years
+ORDER BY OrderDateKey ASC
